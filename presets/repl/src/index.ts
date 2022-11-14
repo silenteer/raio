e15195850c7d07d86883af4c295a78322da3d619
Duke <duc@silentium.io>
2022-11-14 12:11:18 +0700

    feat: repl release

--- presets/repl/src/index.ts
+++ presets/repl/src/index.ts
@@ -4,5 +4,21 @@ import repl from "repl"
 
 export const adaptor = define.adaptor(async (raio, router) => {
 
-  repl.start('> ').context.m = 'hello world'
+  const replServer = repl.start({ prompt: '> ' });
+
+  replServer.context.server = raio
+  replServer.context.router = router
+
+  replServer.defineCommand('inspect', {
+    help: 'inspect the running server',
+    action(name) {
+      replServer.clearBufferedCommand()
+      raio.inspect().then(result => {
+        console.log(result)
+        replServer.displayPrompt()
+      })
+    }
+  })
+
+  setTimeout(() => replServer.displayPrompt(), 50)
 })
